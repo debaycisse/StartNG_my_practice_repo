5daabeb784136eaf900851c1af5eb5001a507eb5
unknown <debaycisse@gmail.com>
2020-03-24 15:25:46 +0100

    PHP task completes. file is created afer form submssion on index.html

--- processform1.php
+++ processform1.php
@@ -3,22 +3,29 @@
 
 
     // Fetching user name
-    $username = $_POST['user_name'];
+    $user_name = $_POST['user_name'];
+
     // Creating filename, using the username
-    $filename = ${"$username".".txt"};
+    $filename =  $user_name.'.php';
+    
+    // $filename = ${"$user_name".".txt"};
+    
     //Opening the file with the username and .txt extension
     $fp = fopen($filename, 'w');
+    
     // Creating content to write into the opened file
     $content = "<h1>Users Contact Details:</h1><br />
     <p>First Name  : $_POST[first_name] <p/>
     <p>Last Name   : $_POST[last_name] <p/>
     <p>Username    : $_POST[user_name] <p/>
     "; 
+    
     // Writing the contents in to the file
     fwrite($fp, $content);
     // Closing the file
     fclose($fp);
-
+    // Display the user's filename and it's location
+    echo "User's file: ", $filename , " created in the current directory." ;
 
     // print_r($_POST)
 
